8d740502e5388e1a9a5e8000141d406cbf79aaca
Zak Maccorquodale <46991887+Zakfd@users.noreply.github.com>
2022-11-20 00:47:06 -0800

    Add files via upload

---
+++ data/get_csv.py
@@ -0,0 +1,57 @@
+import time
+from datetime import datetime
+
+import pandas
+import requests
+
+
+def generate_csv(self, filename):
+    df = pandas.DataFrame(columns=['repository_ID', 'name', 'URL', 'created_date', 'description', 'Language', 'number_of_stars', 'type', 'created_at', 'forks_count'])
+
+    idx_count = 1
+    page_count = 1
+    search_query = '>500'
+
+    while idx_count <= 2500:
+        result = requests.get(
+            f'https://api.github.com/search/repositories?q=stars:{search_query}&sort=stars&order=desc&per_page=100&page={page_count}').json()
+        # print(result)
+        for repo in result['items']:
+            temp = {'repository_ID': repo['id'],
+                    'name': repo['name'],
+                    'URL': repo['html_url'],
+                    'created_date': datetime.strptime(repo['created_at'], '%Y-%m-%dT%H:%M:%SZ'),
+                    'Language': repo['language'],
+                    'number_of_stars': repo['stargazers_count'],
+                    'type': repo['owner']['type'],
+                    'forks_count': repo['forks_count']}
+
+            temp_df = pandas.DataFrame(temp, index=[idx_count])
+            df = pandas.concat([df, temp_df])
+            print(f"Indexed: {temp['name']}")
+            print(f"Number of indexed results: {idx_count}")
+            idx_count = idx_count + 1
+
+            if (idx_count % 1000 == 0):
+                last_stars = repo['stargazers_count']
+                search_query = f"<{last_stars}"
+                print(f"1000 results reached, waiting and starting new query at {last_stars} stars...")
+                time.sleep(360)
+                page_count = 0
+        page_count = page_count + 1
+        print(f"Number of pages completed: {page_count}")
+
+    print(df)
+    df.to_csv('data.csv')
+
+
+if __name__ == '__main__':
+    generate_csv('hello', 'hello')# 'hello's are place holders for when we want to name the data file.
+    ############### this is a little bit in how to get the contributors, though it is a hard coded example for testing#############
+    # idx_count = 1
+    # page_count = 1
+    # search_query = '>500'
+    # result = requests.get(
+    #     f'https://api.github.com/repos/Nukesor/pueue/contributors').json()
+    # for repo in result:
+    #     print(repo)
