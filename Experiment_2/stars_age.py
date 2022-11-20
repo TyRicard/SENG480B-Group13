e6302827484ef474e5272e8ef48329ff3013f6de
TyRicard <tricard.summer@gmail.com>
2022-11-19 22:13:20 -0800

    Adding Experiment 2 Code

---
+++ Experiment_2/stars_age.py
@@ -0,0 +1,38 @@
+import datetime as dt
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Adding Custom Mapping Function
+def subtract_times(past_str):
+    # For Datetime Calculations: https://docs.python.org/3/library/datetime.html
+    # This current_time is technically variable but the program runs fast enough
+    # that the only change for incorrect results is if the value is taken at midnight
+    current_time = dt.datetime.now()
+    past_time = dt.datetime.strptime(past_str, '%Y-%m-%d %H:%M:%S')
+    difference = current_time - past_time
+    # Use floor division
+    return (difference.days // 7)
+
+
+def main():
+    # Basic Seaborn Approach: https://seaborn.pydata.org/tutorial/introduction
+    stars_and_age = pd.read_csv("../data/datasets/data.csv")
+
+    # For element-wise Calculation with Pandas: https://queirozf.com/entries/pandas-dataframe-examples-column-operations
+    stars_and_age["age"] = stars_and_age['created_date'].map(lambda cd: subtract_times(cd))
+
+    # This produces a scatterplot
+    scatter_plot =  sns.scatterplot(
+                    data=stars_and_age,
+                    x="age",
+                    y="number_of_stars")
+
+    # Changing the Label Titles to Match Approach
+    scatter_plot.set(xlabel="Age (Weeks)",
+                    ylabel="Stars")
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ Experiment_2/stars_forks.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def main():
+    # Basic Seaborn Approach: https://seaborn.pydata.org/tutorial/introduction
+    stars_and_forks = pd.read_csv("../data/datasets/data.csv")
+
+    # This produces a scatterplot
+    scatter_plot =  sns.scatterplot(
+                    data=stars_and_forks,
+                    x="forks_count",
+                    y="number_of_stars")
+
+    # Changing the Label Titles to Match Approach
+    scatter_plot.set(xlabel="Forks",
+                    ylabel="Stars")
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
