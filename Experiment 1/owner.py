dbef13d6b5df6681b150976e13a9822affe47a14
Zak Maccorquodale <46991887+Zakfd@users.noreply.github.com>
2022-11-20 17:58:22 -0800

    Add files via upload

---
+++ Experiment 1/owner.py
@@ -0,0 +1,14 @@
+import pandas
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    df = pandas.read_csv('data.csv')
+    df = df.drop(df.iloc[:, :7], axis=1)
+    df = df.drop(df.iloc[:, 2:], axis=1)
+    df = df.dropna()
+
+    plot = pandas.DataFrame({col: vals['number_of_stars'] for col, vals in df.groupby(by=['type'])})
+    med = plot.median().sort_values(ascending=False)
+    box = plot[med.index].boxplot(rot=15, figsize=(10, 10), showfliers=False)
+
+    plt.show()
