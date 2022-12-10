232ac001f673cdf21d2c5ab9da68bae4b0a60880
Zak Maccorquodale <46991887+Zakfd@users.noreply.github.com>
2022-12-10 14:50:15 -0800

    Create star_issue.py

---
+++ Experiment_2/star_issue.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def main():
+    # Basic Seaborn Approach: https://seaborn.pydata.org/tutorial/introduction
+    stars_and_commits = pd.read_csv("../data/datasets/data.csv")
+
+    # This produces a scatterplot
+    scatter_plot =  sns.scatterplot(
+                    data=stars_and_issues,
+                    x="issue_count",
+                    y="number_of_stars")
+
+    # Changing the Label Titles to Match Approach
+    scatter_plot.set(xlabel="Issue",
+                    ylabel="Stars",
+                    xscale="log",
+                    yscale="log",
+                    ylim=1000)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
